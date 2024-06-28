016e49b449d20175b054760132fc788973d9ac95
Alisson Carvalho <alissonanderson2013@gmail.com>
2024-06-28 20:28:53 -0300

    formulário

--- script.js
+++ script.js
@@ -18,31 +18,31 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
 
         // Enviar o formulário para o Formspree
-        fetch(this.action, {
+        fetch('https://formspree.io/f/xqazzroz', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json'
             },
             body: new FormData(this)
         })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Houve um problema ao enviar o formulário.');
-                }
-                document.getElementById('form-feedback').classList.remove('hidden');
-                // Limpa os campos do formulário após o envio
-                document.getElementById('name').value = '';
-                document.getElementById('email').value = '';
-                document.getElementById('message').value = '';
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Houve um problema ao enviar o formulário.');
+            }
+            document.getElementById('form-feedback').classList.remove('hidden');
+            // Limpa os campos do formulário após o envio
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('message').value = '';
 
-                // Oculta a mensagem de feedback após 3 segundos
-                setTimeout(function () {
-                    document.getElementById('form-feedback').classList.add('hidden');
-                }, 3000);
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                alert('Houve um erro ao enviar o email. Por favor, tente novamente.');
-            });
+            // Oculta a mensagem de feedback após 3 segundos
+            setTimeout(function () {
+                document.getElementById('form-feedback').classList.add('hidden');
+            }, 3000);
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+            alert('Houve um erro ao enviar o email. Por favor, tente novamente.');
+        });
     });
 });
