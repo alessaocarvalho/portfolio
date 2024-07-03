c0a1f454da681687a35a08baaa6ef6c2769ed3ca
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2024-07-03 16:42:05 -0300

    botão voltar ao topo

--- script.js
+++ script.js
@@ -75,4 +75,18 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > document.querySelector('.hero').offsetHeight) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
